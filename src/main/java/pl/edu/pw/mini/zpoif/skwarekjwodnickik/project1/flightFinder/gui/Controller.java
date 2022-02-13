ec79ba85b82c011be6bac41d031cf96ca1833f98
Krzysztof Wodnicki <woodoo650@gmail.com>
2022-02-13 23:54:17 +0100

    airport search

--- src/main/java/pl/edu/pw/mini/zpoif/skwarekjwodnickik/project1/flightFinder/gui/Controller.java
+++ src/main/java/pl/edu/pw/mini/zpoif/skwarekjwodnickik/project1/flightFinder/gui/Controller.java
@@ -1,13 +1,12 @@
 package pl.edu.pw.mini.zpoif.skwarekjwodnickik.project1.flightFinder.gui;
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
-import pl.edu.pw.mini.zpoif.skwarekjwodnickik.project1.flightFinder.api.services.FlightsServicesStandard;
+import javafx.scene.control.TextField;
 import pl.edu.pw.mini.zpoif.skwarekjwodnickik.project1.flightFinder.model.Converters;
 
 import java.io.FileNotFoundException;
@@ -22,6 +21,12 @@ public class Controller {
     private ListView<String> origin;
 
     @FXML
+    private TextField destinationSearch;
+
+    @FXML
+    private TextField originSearch;
+
+    @FXML
     private DatePicker dateEnd;
 
     @FXML
@@ -33,12 +38,30 @@ public class Controller {
     @FXML
     private void initialize() throws FileNotFoundException, URISyntaxException {
         ObservableList<String> airports = Converters.allIcaos();
+        FilteredList<String> filteredAirportsO = new FilteredList<>(airports, s->true);
+        FilteredList<String> filteredAirportsD = new FilteredList<>(airports, s->true);
 
-        origin.getItems().clear();
-        origin.getItems().addAll(airports);
+        originSearch.textProperty().addListener(obs->{
+            String filter = originSearch.getText().toUpperCase();
+            if(filter.length() == 0) {
+                filteredAirportsO.setPredicate(s -> true);
+            }
+            else {
+                filteredAirportsO.setPredicate(s -> s.contains(filter));
+            }
+        });
+        origin.setItems(filteredAirportsO);
 
-        destination.getItems().clear();
-        destination.getItems().addAll(airports);
+        destinationSearch.textProperty().addListener(obs->{
+            String filter = destinationSearch.getText().toUpperCase();
+            if(filter.length() == 0) {
+                filteredAirportsD.setPredicate(s -> true);
+            }
+            else {
+                filteredAirportsD.setPredicate(s -> s.contains(filter));
+            }
+        });
+        destination.setItems(filteredAirportsD);
     }
 
     @FXML
@@ -48,10 +71,6 @@ public class Controller {
 
         resultList.getItems().clear();
         resultList.getItems().addAll(originValue, destinationValue);
-
-        //FlightsServicesStandard fss = new FlightsServicesStandard();
-        //fss.getFlightsFromBeginToEnd(Converters.RomanToUnix(Converters.localDatetoDate(dateStart.getValue())),
-        //        Converters.RomanToUnix(Converters.localDatetoDate(dateEnd.getValue())));
     }
 
 }
