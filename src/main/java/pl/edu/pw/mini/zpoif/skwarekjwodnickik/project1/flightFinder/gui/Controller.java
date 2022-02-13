32b0f70d246c371938a41c2eed119283a20d3f4e
janskwr <janskwarek@protonmail.ch>
2022-02-13 22:00:46 +0100

    Added CSV convertion, airport data and new convertors.

--- src/main/java/pl/edu/pw/mini/zpoif/skwarekjwodnickik/project1/flightFinder/gui/Controller.java
+++ src/main/java/pl/edu/pw/mini/zpoif/skwarekjwodnickik/project1/flightFinder/gui/Controller.java
@@ -6,6 +6,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
+import pl.edu.pw.mini.zpoif.skwarekjwodnickik.project1.flightFinder.model.Converters;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
 
 public class Controller {
 
@@ -25,11 +29,12 @@ public class Controller {
     private ListView<String> resultList;
 
     @FXML
-    private void initialize() {
-        ObservableList<String> airports = Tmp.getCodes();
+    private void initialize() throws FileNotFoundException, URISyntaxException {
+//        Converters converters = new Converters();
+//        ObservableList<String> airports = Converters.allIcaos();
 
-        origin.setItems(airports);
-        destination.setItems(airports);
+//        origin.setItems(airports);
+//        destination.setItems(airports);
     }
 
     @FXML
